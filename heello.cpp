1f7844c7856355a63149bfce11b251e769328cec
DarshanPatidar11 <darshanpatidar1102@gmail.com>
2025-03-21 12:34:00 +0100

    uba

--- heello.cpp
+++ heello.cpp
@@ -56,3 +56,32 @@ cin.tie(NULL);
     
     return 0;
 }
+
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define f(i,a,b) for (ll i=a;i<b;i++)
+#define pb push_back
+#define vl vector<ll>
+#define vvl vector<vector<ll>>
+#define vpl vector<pair<ll,ll>>
+#define F first
+#define S second
+#define show(a,n) for(ll i=0;i<n;i++) {cout<<a[i]<<" ";} cout<<endl;
+#define cina(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define N 1000000007
+#define pi 3.141592653589793238462643383279502884197169399
+
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+    ll t; cin>>t;
+    for(int as=0;as<t;as++){
+        ll n; cin>>n;
+        
+    }
+    
+    return 0;
+}
